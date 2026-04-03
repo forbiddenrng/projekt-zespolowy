c79eac5b3b44b582a091c1152c9449380df7f5f2
Antek Gawron <antek.gawron@gmail.com>
2026-04-03 21:09:30 +0200

    add integration tests for user service

--- ai-service/tests/integration/test_user_service.py
+++ ai-service/tests/integration/test_user_service.py
@@ -26,6 +26,17 @@ class TestUserServiceIntegration:
   @pytest.fixture
   def user_id(self):
     return TEST_USERS[0]["id"]
+  
+  @pytest.fixture
+  def delete_work_experience(self, user_id):
+    headers = {"x-user": json.dumps({"id": user_id})}
+    response = requests.post(
+        f"{USER_SERVICE_URL}/users/work-experience",
+        headers=headers,
+        json={"workExperiences": []},
+        timeout=10,
+    )
+
 
   def test_get_user_profile_exists(self, wait_for_services, user_id):
     """Test getting user profile existence."""
@@ -55,3 +66,121 @@ class TestUserServiceIntegration:
     assert response.status_code == 200
     assert data["data"] is not None
     assert isinstance(data["data"]["exists"], bool)
+
+
+
+  def test_get_user_work_experience(self, wait_for_services, user_id):
+    """Test getting user work experience from /users/work-experiences"""
+    headers = {"x-user": json.dumps({"id": user_id})}
+    response = requests.get(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        timeout=10,
+    )
+    data = response.json()
+    assert response.status_code == 200
+    assert data["data"] is not None
+    assert isinstance(data["data"], list)
+
+  def test_get_user_work_experience_nonexisting_user(self, wait_for_services, user_id):
+    """Test getting user work experience from /users/work-experiences"""
+    headers = {"x-user": json.dumps({"id": "nonexisting|id"})}
+    response = requests.get(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        timeout=10,
+    )
+    data = response.json()
+    assert response.status_code == 404
+
+
+  def test_update_user_profile(self, wait_for_services, user_id):
+    """Test updating user profile"""
+    headers = {"x-user": json.dumps({"id": user_id})}
+    response = requests.patch(
+        f"{USER_SERVICE_URL}/users/me",
+        headers=headers,
+        json={
+          "profileSummary": "New profile summary over 20 chars"
+        },
+        timeout=10,
+    )
+    data = response.json()
+    assert response.status_code == 200
+
+  def test_update_user_profile(self, wait_for_services, user_id):
+    """Test updating user email - wrong field"""
+    headers = {"x-user": json.dumps({"id": user_id})}
+    response = requests.patch(
+        f"{USER_SERVICE_URL}/users/me",
+        headers=headers,
+        json={
+          "email": "newemail@email.com"
+        },
+        timeout=10,
+    )
+    data = response.json()
+    assert response.status_code == 400
+
+  def test_create_user_work_experience(self, wait_for_services, delete_work_experience, user_id):
+    """Test creating user work experience"""
+    headers = {"x-user": json.dumps({"id": user_id})}
+    response = requests.put(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        json={
+          "workExperiences": [{"companyName": "Example Company", "position": "Intern", "beginDate": "2026-01-01", "description": "Example work experience description"}]
+        },
+        timeout=10,
+    )
+    data = response.json()
+    assert response.status_code == 200
+
+    ## test integrity 
+    response = requests.get(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        timeout=10,
+    )
+    data = response.json()
+    assert data["data"][0]["company_name"] == "Example Company"
+
+
+  def test_update_user_work_experience(self, wait_for_services, delete_work_experience, user_id):
+    """Test updating user work experience from /users/work-experiences"""
+    headers = {"x-user": json.dumps({"id": user_id})}
+    response = requests.put(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        json={
+          "workExperiences": [{"companyName": "Example Company", "position": "Intern", "beginDate": "2026-01-01", "description": "Example work experience description"}]
+        },
+        timeout=10,
+    )
+    data = response.json()
+    assert response.status_code == 200
+    work_id = data["data"][0]["id"]
+
+    ## update 
+    response = requests.put(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        json={
+          "workExperiences": [{"id": work_id, "companyName": "Example Company New", "position": "Intern", "beginDate": "2026-01-01", "description": "Example work experience description"}]
+        },
+        timeout=10,
+    )
+
+    assert response.status_code == 200
+
+    response = requests.get(
+        f"{USER_SERVICE_URL}/users/work-experiences",
+        headers=headers,
+        timeout=10,
+    )
+
+    data = response.json()
+    assert data["data"][0]["id"] == work_id
+    assert data["data"][0]["company_name"] == "Example Company New"
+
+
